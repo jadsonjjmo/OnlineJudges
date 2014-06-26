5f58ccd913d9b1edff639884f28eacfaa6b53f61
jadsonjjmo <jadsonjjmo@gmail.com>
2014-06-26 18:06:29 -0300

    Some dois Números

--- SPOJ_BR/19776.java
+++ SPOJ_BR/19776.java
@@ -13,8 +13,8 @@ public class 19776 {
 		StringBuilder sb = new StringBuilder();
 		
 		char carac[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A','B',
-				'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N',
-				'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+				 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N',
+			       'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 		
 		int aux;
 		while (true) {
